ac1c4e06bd5d74806c940303cb07c9aa371876a6
tiannian <dtiannian@gmail.com>
2021-09-17 16:29:52 +0800

    add utxo module

--- findorad/src/main.rs
+++ findorad/src/main.rs
@@ -1,1 +1,21 @@
+#![feature(generic_associated_types)]
+
+mod utxo;
+mod transaction;
+
+use transaction::FindoraTransaction;
+
+use sha3::Sha3_512;
+
+#[abcf::manager(
+    name = "findorad",
+    digest = "sha3::Sha3_512",
+    version = 0,
+    impl_version = "1.0.0",
+    transaction = "FindoraTransaction"
+)]
+pub struct SimpleManager {
+    pub utxo: utxo::UTXO,
+}
+
 fn main() {}
