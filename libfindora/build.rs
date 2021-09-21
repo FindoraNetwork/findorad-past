51afe79c87622eb635f9a6ba4eb1325eb8bca5f0
tiannian <dtiannian@gmail.com>
2021-09-22 05:09:54 +0800

    add capnp

--- libfindora/build.rs
+++ libfindora/build.rs
@@ -1,6 +1,6 @@
 fn main() {
     capnpc::CompilerCommand::new()
-        // .src_prefix("schema")
+        .src_prefix("schema")
         .file("schema/transaction.capnp")
         .run().expect("schema compiler command");
 }
--- libfindora/src/lib.rs
+++ libfindora/src/lib.rs
@@ -1,3 +1,8 @@
 pub mod coinbase;
 pub mod transaction;
 pub mod utxo;
+
+pub mod transaction_capnp {
+    include!(concat!(env!("OUT_DIR"), "/transaction_capnp.rs"));
+}
+
