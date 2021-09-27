ed775636bf831228b5e987f02e8b11ecba6d81da
tiannian <dtiannian@gmail.com>
2021-09-28 05:31:16 +0800

    add owner memos

--- cli/src/entry/mod.rs
+++ cli/src/entry/mod.rs
@@ -1,24 +1,61 @@
+use libfindora::transaction::{Input, Output, Transaction};
 use zei::xfr::sig::{XfrKeyPair, XfrPublicKey};
 use serde::{Deserialize, Serialize};
 use zei::xfr::structs::XfrAssetType;
+use ruc::*;
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct IssueBatchEntry {
+pub struct IssueEntry {
     pub keypair: XfrKeyPair,
     pub amount: u64,
     pub asset_type: XfrAssetType,
+    pub confidential_amount: bool,
+}
+
+impl IssueEntry {
+    pub fn build(self) -> (Vec<Input>, Vec<Output>) {
+        let inputs = Vec::new();
+        let outputs = Vec::new();
+
+        (inputs, outputs)
+    }
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct TransferBatchEntry {
+pub struct TransferEntry {
     pub from: XfrKeyPair,
     pub to: XfrPublicKey,
     pub amount: u64,
     pub asset_type: XfrAssetType,
+    pub confidential_amount: bool,
+    pub confidential_asset: bool,
+}
+
+impl TransferEntry {
+    pub fn build(self) -> (Vec<Input>, Vec<Output>) {
+        let inputs = Vec::new();
+        let outputs = Vec::new();
+
+        (inputs, outputs)
+    }
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct BalanceEntry {
-    pub amount: u64,
-    pub balance: u64,
+pub enum Entry {
+    Issue(IssueEntry),
+    Transfer(TransferEntry),
 }
+
+pub fn build_transaction(entries: Vec<Entry>) -> Result<Transaction> {
+    let tx = Transaction::default();
+
+    for entry in entries {
+        let (inputs, outputs) = match entry {
+            Entry::Issue(e) => e.build(),
+            Entry::Transfer(e) => e.build(),
+        };
+    }
+
+    Ok(tx)
+}
+
