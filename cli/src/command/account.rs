ed7bca4b7257caf36c8d8cf11d2151d737ae76de
tommady <tommady@users.noreply.github.com>
2021-12-15 13:36:33 +0800

    command account done

--- cli/src/command/account.rs
+++ cli/src/command/account.rs
@@ -1,14 +1,17 @@
 use clap::{ArgGroup, Parser};
-use ruc::Result;
 
 use crate::config::Config;
 
 #[derive(Parser, Debug)]
-#[clap(group = ArgGroup::new("account"))]
-pub struct Command {}
+#[clap(group = ArgGroup::new("account").required(true).args(&["addr"]))]
+pub struct Command {
+    /// Findora account(fra1rkv...) or Ethereum address(0xd3Bf...)
+    #[clap(value_name = "address")]
+    addr: String,
+}
 
 impl Command {
-    pub async fn execute(&self, config: Config) -> ruc::Result<()> {
+    pub async fn execute(&self, _config: Config) -> ruc::Result<()> {
         Ok(())
     }
 }
