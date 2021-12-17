37af20fd5694b5cb841edb3bf059f30266d93129
tommady <tommady@users.noreply.github.com>
2021-12-17 12:26:47 +0800

    clean up comments

--- cli/src/command/transfer.rs
+++ cli/src/command/transfer.rs
@@ -1,23 +1,6 @@
-// use std::convert::TryInto;
-
 use clap::{Parser, ValueHint};
-// use rand_chacha::ChaChaRng;
-// use rand_core::SeedableRng;
-// use ruc::{d, eg};
-// use zei::{
-//     serialization::ZeiFromToBytes,
-//     xfr::{
-//         // sig::{XfrPublicKey, XfrSecretKey},
-//         sig::XfrSecretKey,
-//         structs::{AssetType, ASSET_TYPE_LENGTH},
-//     },
-// };
+
 use crate::config::Config;
-// use crate::{
-//     config::Config,
-//     utils::{account_to_keypair, read_account_list, send_tx},
-// };
-// use libfn::{build_transaction, Entry, TransferEntry};
 
 #[derive(Parser, Debug)]
 pub struct Command {
@@ -89,74 +72,12 @@ struct Show {
 
 impl Command {
     pub async fn execute(&self, _config: Config) -> ruc::Result<()> {
-        // use ruc::*;
-
-        // let from = if let Some(from_secret_key) = self.from_secret_key.as_ref() {
-        //     let from_sk_bytes = base64::decode(from_secret_key).c(d!())?;
-        //     let from_sk = XfrSecretKey::zei_from_bytes(&from_sk_bytes)?;
-        //     Some(from_sk.into_keypair())
-        // } else if let Some(account_index) = self.account_index {
-        //     let mut kp = None;
-        //     let v = read_account_list(&config).await?;
-        //     if let Some(account) = v.get(account_index) {
-        //         let result = account_to_keypair(account);
-        //         if result.is_err() {
-        //             return Err(result.unwrap_err());
-        //         }
-        //         kp = result.ok();
-        //     }
-        //     kp
-        // } else {
-        //     return Err(eg!("keypair is none"));
-        // };
-
-        // let mut prng = ChaChaRng::from_entropy();
-
-        // let asset_type_bytes = base64::decode(&self.asset_type).c(d!())?;
-        // let asset_type_array: [u8; ASSET_TYPE_LENGTH] = asset_type_bytes
-        //     .try_into()
-        //     .map_err(|e| eg!(format!("{:?}", e)))?;
-        // let asset_type = AssetType(asset_type_array);
-
-        // let to_pk_bytes = base64::decode(&self.to_public_key).c(d!())?;
-        // let to = XfrPublicKey::zei_from_bytes(&to_pk_bytes)?;
-
-        // if let Some(_b) = &self.batch {
-        // } else {
-        //     let entry = Entry::Transfer(TransferEntry {
-        //         confidential_amount: self.confidential_amount,
-        //         confidential_asset: self.confidential_asset,
-        //         amount: self.amount,
-        //         asset_type,
-        //         from: from.unwrap(), //safe
-        //         to,
-        //     });
-
-        //     let tx = build_transaction(&mut prng, vec![entry]).await?;
-        //     log::debug!("Result tx is: {:?}", tx);
-
-        //     send_tx(&tx).await?;
-        // }
-        //
-        // ++++++++++++++++++++++++++++
-        // the original execute.rs code
-        // ++++++++++++++++++++++++++++
-        // let mut prng = ChaChaRng::from_entropy();
-
-        // let list = read_list(&config, &self.batch_name).await?;
-
-        // let tx = build_transaction(&mut prng, list).await?;
-
-        // if self.dump_transaction {
-        //     println!("{:#?}", tx);
-        // } else {
-        //     log::debug!("Result tx is: {:?}", tx);
-
-        //     send_tx(&tx).await?;
-
-        //     clean_list(&config, &self.batch_name).await?;
-        // }
-
+        match &self.subcmd {
+            SubCommand::Send(_send) => {}
+            SubCommand::Save(_save) => {}
+            SubCommand::Batch(_batch) => {}
+            SubCommand::Show(_show) => {}
+        }
         Ok(())
     }
 }
--- cli/src/command/wallet.rs
+++ cli/src/command/wallet.rs
@@ -1,15 +1,4 @@
 use clap::{ArgEnum, Parser};
-// use ruc::d;
-// use zei::{serialization::ZeiFromToBytes, xfr::sig::XfrSecretKey};
-//
-// use crate::{
-//     config::Config,
-//     utils::{
-//         account_to_keypair, delete_account_one, get_value_map, read_account_list,
-//         write_account_list,
-//     },
-// };
-// use libfn::AccountEntry;
 
 use crate::config::Config;
 
@@ -68,70 +57,6 @@ impl Command {
             SubCommand::Create(_create) => {}
             SubCommand::Delete(_delete) => {}
         }
-        // use ruc::*;
-
-        // if self.generate {
-        //     let entry = AccountEntry::generate_keypair()?;
-        //     println!("{:#?}", entry);
-        //     write_account_list(&config, vec![entry], false).await?;
-        //     return Ok(());
-        // }
-
-        // if self.list {
-        //     let v = read_account_list(&config).await?;
-        //     println!("{:#?}", v);
-        //     return Ok(());
-        // }
-
-        // if let Some(index) = self.delete {
-        //     let entry = delete_account_one(&config, index).await?;
-        //     println!("{:#?}", entry);
-        //     return Ok(());
-        // }
-
-        // if let Some(phrase) = self.add_mnemonic.as_ref() {
-        //     let entry = AccountEntry::generate_keypair_from_mnemonic(phrase)?;
-        //     println!("{:#?}", entry);
-
-        //     write_account_list(&config, vec![entry], false).await?;
-        //     return Ok(());
-        // }
-
-        // if self.show {
-        //     let mut wallets = vec![];
-
-        //     if let Some(index) = &self.account_index {
-        //         let v = read_account_list(&config).await?;
-        //         let account = v
-        //             .get(*index)
-        //             .ok_or(d!(format!("the index not exist, array len:{}", v.len())))?;
-        //         let kp = account_to_keypair(account)?;
-        //         wallets.push(kp);
-        //     }
-
-        //     if let Some(w) = &self.wallet {
-        //         let sk_bytes = base64::decode(&w).c(d!())?;
-        //         let sk = XfrSecretKey::zei_from_bytes(&sk_bytes)?;
-
-        //         let keypair = sk.into_keypair();
-        //         wallets.push(keypair);
-        //     }
-
-        //     if wallets.len() == 0 {
-        //         return Err(Box::from(d!(
-        //             "must set wallet(base64) or wallet-index(usize)"
-        //         )));
-        //     }
-
-        //     let value_map = get_value_map(wallets).await?;
-
-        //     for (k, amount) in value_map.iter() {
-        //         let asset_type = base64::encode(&k.zei_to_bytes());
-
-        //         println!("Asset type: {}, amount: {}", asset_type, amount);
-        //     }
-        // }
-
         Ok(())
     }
 }
