cf9e3dd4ac83ed4e5b36c858353d116335ebc881
tommady <tommady@users.noreply.github.com>
2021-12-20 15:37:24 +0800

    command wallet done

--- cli/src/command/wallet.rs
+++ cli/src/command/wallet.rs
@@ -1,7 +1,9 @@
 use crate::config::Config;
+use crate::entry::wallet as entry_wallet;
 
-use anyhow::Result;
+use anyhow::{anyhow, bail, Context, Result};
 use clap::Parser;
+use libfn::types::Wallet as lib_wallet;
 
 #[derive(Parser, Debug)]
 pub struct Command {
@@ -31,6 +33,9 @@ struct Create {
     /// Specific to create a new wallet from Mnemonic phrase
     #[clap(short, long, forbid_empty_values = true)]
     mnemonic: Option<String>,
+    /// Name of the new wallet to be created
+    #[clap(short, long, forbid_empty_values = true)]
+    name: Option<String>,
 }
 
 #[derive(Parser, Debug)]
@@ -41,12 +46,65 @@ struct Delete {
 }
 
 impl Command {
-    pub fn execute(&self, _config: Config) -> Result<()> {
+    pub fn execute(&self, cfg: &Config) -> Result<()> {
+        let mut wallets = entry_wallet::Wallets::new(&cfg.node.home)
+            .with_context(|| format!("wallets new failed: {:?}", cfg.node.home))?;
+
         match &self.subcmd {
-            SubCommand::Show(_show) => {}
-            SubCommand::Create(_create) => {}
-            SubCommand::Delete(_delete) => {}
+            SubCommand::Show(cmd) => show(cmd, &wallets)?,
+            SubCommand::Create(cmd) => create(cmd, &mut wallets)?,
+            SubCommand::Delete(cmd) => delete(cmd, &mut wallets)?,
         }
         Ok(())
     }
+}
+
+fn show(cmd: &Show, wallets: &entry_wallet::Wallets) -> Result<()> {
+    let _result = match &cmd.address {
+        Some(a) => vec![wallets
+            .read(a)
+            .with_context(|| format!("read wallet failed: {:?}", cmd))?],
+        None => wallets.list(),
+    };
+
+    Ok(())
+}
+
+fn delete(cmd: &Delete, wallets: &mut entry_wallet::Wallets) -> Result<()> {
+    wallets
+        .delete(&cmd.address)
+        .with_context(|| format!("delete wallet failed: {:?}", cmd))
+}
+
+fn create(cmd: &Create, wallets: &mut entry_wallet::Wallets) -> Result<()> {
+    let result = match &cmd.mnemonic {
+        Some(m) => lib_wallet::from_mnemonic(&m),
+        None => lib_wallet::generate(),
+    };
+
+    let wallet = match result {
+        Ok(v) => v,
+        Err(e) => bail!("lib_wallet creating failed: {:?}", e),
+    };
+
+    wallets
+        .create(&entry_wallet::Wallet {
+            name: cmd.name.clone(),
+            mnemonic: wallet.mnemonic,
+            address: wallet
+                .address
+                .to_base64()
+                .map_err(|e| anyhow!("lib_wallet address to_base64 failed: {}", e))?,
+            public: wallet
+                .public
+                .to_base64()
+                .map_err(|e| anyhow!("lib_wallet public to_base64 failed: {}", e))?,
+            secret: wallet
+                .secret
+                .to_base64()
+                .map_err(|e| anyhow!("lib_wallet secret to_base64 failed: {}", e))?,
+        })
+        .with_context(|| format!("create wallet failed: {:?}", cmd))?;
+
+    Ok(())
 }
