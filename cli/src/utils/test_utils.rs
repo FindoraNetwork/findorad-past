0fadfc85171ebf2756f6b3d302b0b562df39931e
tommady <tommady@users.noreply.github.com>
2021-12-20 09:31:01 +0800

    adding test_util.rs

---
+++ cli/src/utils/test_utils.rs
@@ -0,0 +1,47 @@
+use std::{
+    env::temp_dir,
+    fs,
+    path::{Path, PathBuf},
+};
+
+use anyhow::{Context, Result};
+
+pub struct TempDir {
+    path: Option<PathBuf>,
+}
+
+impl TempDir {
+    pub fn new<P: Into<PathBuf>>(path: P) -> Result<TempDir> {
+        let p = path.into();
+        let dir = temp_dir().as_path().join(p);
+        fs::create_dir_all(dir.clone())
+            .with_context(|| format!("failed to create directory: {}", dir.display()))?;
+
+        Ok(TempDir { path: Some(dir) })
+    }
+
+    pub fn path(&self) -> &Path {
+        self.path
+            .as_ref()
+            .expect("temp dir has already been removed")
+    }
+
+    pub fn remove(&mut self) {
+        if let Some(p) = &self.path {
+            let _ = fs::remove_dir_all(p);
+            self.path = None;
+        }
+    }
+}
+
+impl Drop for TempDir {
+    fn drop(&mut self) {
+        self.remove();
+    }
+}
+
+impl AsRef<Path> for TempDir {
+    fn as_ref(&self) -> &Path {
+        self.path()
+    }
+}
