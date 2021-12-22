6d42cdd1b729df73e665d848b6f2f90b47ced265
tommady <tommady@users.noreply.github.com>
2021-12-22 16:07:00 +0800

    feat-v1-address-cli-config-refactor (#44)
    * removing redundent config path
    
    * removing redundent home path from node
    
    * let the default home path more robust
    
    * let the config test case more sense
    
    * fix clippy linters

--- cli/src/config/node.rs
+++ cli/src/config/node.rs
@@ -1,23 +1,14 @@
-use std::path::PathBuf;
-
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Node {
     pub address: String,
-    pub home: PathBuf,
 }
 
 impl Default for Node {
     fn default() -> Node {
-        // must get "home"
-        let mut home = home::home_dir().unwrap();
-        home.push(".findora");
-        home.push("fn");
-
         Node {
             address: String::from("http://localhost:25576"),
-            home,
         }
     }
 }
