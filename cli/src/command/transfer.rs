068783027a3a2bcb86a14582688eb429c556c659
tommady <tommady@users.noreply.github.com>
2021-12-24 09:13:14 +0800

    change transfer send accepting args

--- cli/src/command/transfer.rs
+++ cli/src/command/transfer.rs
@@ -3,7 +3,7 @@ use std::fmt::Display;
 use crate::config::Config;
 
 use anyhow::Result;
-use clap::{Parser, ValueHint};
+use clap::{ArgGroup, Parser};
 
 #[derive(Parser, Debug)]
 pub struct Command {
@@ -24,22 +24,23 @@ enum SubCommand {
 }
 
 #[derive(Parser, Debug)]
+#[clap(group(ArgGroup::new("from").required(true).args(&["from-address", "from-secret"])))]
 struct Send {
-    /// To specific a file path to the Findora wallet which contains base64-formatted XfrPrivateKey
-    /// This option cannot be used with --from-secret-string
-    #[clap(group = "from")]
-    #[clap(short = 'f', long, value_name = "PATH", forbid_empty_values = true, value_hint = ValueHint::FilePath)]
-    from_secret_file: Option<std::path::PathBuf>,
-    /// To specific a plain-text input as the Findora wallet which is a base64-formatted XfrPrivateKey
-    /// This option cannot be used with --from-secret-file
-    #[clap(group = "from")]
-    #[clap(short = 's', long, value_name = "STRING", forbid_empty_values = true)]
-    from_secret_string: Option<String>,
+    /// To specific an address as the Findora wallet which is
+    /// 1. ETH compatible address (0x...)
+    /// 2. Findora addreess (fra...)
+    #[clap(short = 'f', long, value_name = "ADDRESS", forbid_empty_values = true)]
+    from_address: Option<String>,
+    /// To specific a plain-text input as the Findora wallet which is a base64-formatted secret
+    #[clap(short = 's', long, value_name = "SECRET", forbid_empty_values = true)]
+    from_secret: Option<String>,
     /// Amount of FRA tokens to send
     #[clap(short, long, required = true, forbid_empty_values = true)]
     amount: u64,
-    /// Address to send which is a base64-formated XfrPublicKey
-    #[clap(forbid_empty_values = true)]
+    /// Address to send which is a
+    /// 1. ETH compatible address (0x...) or
+    /// 2. Findora addreess (fra...)
+    #[clap(short, long, required = true, forbid_empty_values = true)]
     to_address: String,
     /// Make the amount confidential in the transaction
     #[clap(short = 'A', long)]
@@ -76,11 +77,26 @@ struct Show {
 impl Command {
     pub fn execute(&self, _config: Config) -> Result<Box<dyn Display>> {
         match &self.subcmd {
-            SubCommand::Send(_send) => {}
-            SubCommand::Save(_save) => {}
-            SubCommand::Batch(_batch) => {}
-            SubCommand::Show(_show) => {}
+            SubCommand::Send(cmd) => send(cmd),
+            SubCommand::Save(cmd) => save(cmd),
+            SubCommand::Batch(cmd) => batch(cmd),
+            SubCommand::Show(cmd) => show(cmd),
         }
-        Ok(Box::new(0))
     }
+}
+
+fn send(cmd: &Send) -> Result<Box<dyn Display>> {
+    Ok(Box::new(0))
+}
+
+fn save(cmd: &Save) -> Result<Box<dyn Display>> {
+    Ok(Box::new(0))
+}
+
+fn batch(cmd: &Batch) -> Result<Box<dyn Display>> {
+    Ok(Box::new(0))
+}
+
+fn show(cmd: &Show) -> Result<Box<dyn Display>> {
+    Ok(Box::new(0))
 }
