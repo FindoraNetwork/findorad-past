7e52bcec7f74159bf89f84c3975ca08f7d377cd5
tommady <tommady@users.noreply.github.com>
2022-01-05 14:33:31 +0800

    adding regex to check setup rpc addr

--- cli/src/command/setup.rs
+++ cli/src/command/setup.rs
@@ -2,25 +2,43 @@ use std::fmt::Display;
 
 use crate::config::Config;
 
-use anyhow::Result;
+use anyhow::{bail, Result};
 use clap::Parser;
+use lazy_static::lazy_static;
+use regex::Regex;
+
+lazy_static! {
+    static ref RPC_ADDR_REGEX: Regex =
+        Regex::new(r"^(?:http(s)?://)?[\w.-]+(?:\.[\w\.-]+)*:\d*").unwrap();
+}
 
 #[derive(Parser, Debug)]
 pub struct Command {
     #[clap(short, long)]
     /// Set the address of Findora rpc server
-    set_server_address: Option<String>,
+    set_rpc_server_address: Option<String>,
 }
 
 impl Command {
     pub fn execute(&self, config: Config) -> Result<Box<dyn Display>> {
         let mut config = config;
 
-        if let Some(addr) = &self.set_server_address {
+        if let Some(addr) = &self.set_rpc_server_address {
+            if !is_address_validate(addr) {
+                bail!("address is not valid: {}", addr)
+            }
             config.node.address = addr.clone();
             config.save()?;
         }
 
         Ok(Box::new(0))
     }
+}
+
+// using a simple regex checking instead of using url crate,
+// because
+// 1. the rpc address setup comes only in here and it's format is simple
+// 2. regex crate can be re-used in other places but url crate is not
+fn is_address_validate(addr: &str) -> bool {
+    RPC_ADDR_REGEX.is_match(addr)
 }
