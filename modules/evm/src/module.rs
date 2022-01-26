78d2953f80b752439124b1d4851520ad5317d610
tiannian <dtiannian@gmail.com>
2022-01-26 09:46:07 +0800

    add chain id

--- modules/evm/src/module.rs
+++ modules/evm/src/module.rs
@@ -4,16 +4,18 @@ use abcf::{
         model::{DoubleKeyMap, Map, Value},
     },
     module::types::{RequestCheckTx, RequestDeliverTx, ResponseCheckTx, ResponseDeliverTx},
-    Application, TxnContext,
+    Application, RPCResponse, TxnContext, RPCContext,
 };
 use fm_utxo::UtxoModule;
 use primitive_types::{H160, H256, U256};
 
-use crate::{evm::account::Account, Transaction};
+use crate::{evm::{account::Account, vicinity::Vicinity}, rpc, Transaction};
 
 #[abcf::module(name = "evm", version = 1, impl_version = "0.1.1", target_height = 0)]
 #[dependence(utxo = "UtxoModule")]
 pub struct EvmModule {
+    pub vicinity: Vicinity,
+
     #[stateful(merkle = "AppendOnlyMerkle")]
     pub accounts: Map<H160, Account>,
     #[stateful(merkle = "AppendOnlyMerkle")]
@@ -24,7 +26,20 @@ pub struct EvmModule {
 }
 
 #[abcf::rpcs]
-impl EvmModule {}
+impl EvmModule {
+    pub async fn metadata<'a>(
+        &mut self,
+        _ctx: &mut RPCContext<'a, Self>,
+        _params: rpc::MetadataRequest,
+    ) -> RPCResponse<rpc::MetadataResponse> {
+        let metadata = rpc::MetadataResponse {
+            chain_id: self.vicinity.chain_id.as_u64(),
+            gas_price: self.vicinity.gas_price.as_u64(),
+        };
+
+        RPCResponse::new(metadata)
+    }
+}
 
 /// Module's block logic.
 #[abcf::application]
