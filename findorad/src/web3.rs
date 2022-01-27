fe59b11431cdde8cc0bd02377ffb262ae0fb1ddf
tiannian <dtiannian@gmail.com>
2022-01-27 09:55:47 +0800

    use 0.0.0.0

--- findorad/src/web3.rs
+++ findorad/src/web3.rs
@@ -6,11 +6,11 @@ pub fn strat_web3() {
     let builder = Web3ServerBuilder {
         upstream: "http://127.0.0.1:26657".to_string(),
         #[cfg(feature = "web3-http")]
-        http: "127.0.0.1:8545"
+        http: "0.0.0.0:8545"
             .parse()
             .expect("parse socket address error."),
         #[cfg(feature = "web3-ws")]
-        ws: "127.0.0.1:8546"
+        ws: "0.0.0.0:8546"
             .parse()
             .expect("parse socket address error."),
     };
