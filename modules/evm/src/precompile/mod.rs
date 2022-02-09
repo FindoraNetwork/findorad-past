1e0b4e7d5222107c6e795c7982f12434f16ee806
tiannian <dtiannian@gmail.com>
2022-02-09 20:54:49 +0800

    add precompile

--- modules/evm/src/precompile/mod.rs
+++ modules/evm/src/precompile/mod.rs
@@ -1,10 +1,17 @@
 use std::collections::BTreeMap;
 
-use evm::executor::stack::{PrecompileFn, PrecompileSet, PrecompileOutput, PrecompileFailure, };
+use evm::executor::stack::{PrecompileFailure, PrecompileFn, PrecompileOutput, PrecompileSet};
+use precompile_blake2::Blake2F;
+use precompile_bn128::{Bn128Add, Bn128Mul, Bn128Pairing};
 use precompile_core::Precompile;
-use precompile_simple::{Sha256, ECRecover, Ripemd160, Identity};
+use precompile_curve25519::{Curve25519Add, Curve25519ScalarMul};
+use precompile_ed25519::Ed25519Verify;
+use precompile_modexp::Modexp;
+use precompile_sha3fips::Sha3FIPS256;
+use precompile_simple::{ECRecover, ECRecoverPublicKey, Identity, Ripemd160, Sha256};
 use primitive_types::H160;
 
+#[derive(Default)]
 pub struct Precompiles {
     pub set: BTreeMap<H160, PrecompileFn>,
 }
@@ -16,6 +23,19 @@ impl Precompiles {
         set.insert(u64_to_h160(2), Sha256::execute as PrecompileFn);
         set.insert(u64_to_h160(3), Ripemd160::execute as PrecompileFn);
         set.insert(u64_to_h160(4), Identity::execute as PrecompileFn);
+        set.insert(u64_to_h160(5), Modexp::execute as PrecompileFn);
+        set.insert(u64_to_h160(6), ECRecoverPublicKey::execute as PrecompileFn);
+        set.insert(u64_to_h160(7), Sha3FIPS256::execute as PrecompileFn);
+        set.insert(u64_to_h160(1024), Blake2F::execute as PrecompileFn);
+        set.insert(u64_to_h160(1025), Bn128Pairing::execute as PrecompileFn);
+        set.insert(u64_to_h160(1026), Bn128Add::execute as PrecompileFn);
+        set.insert(u64_to_h160(1027), Bn128Mul::execute as PrecompileFn);
+        set.insert(u64_to_h160(1028), Curve25519Add::execute as PrecompileFn);
+        set.insert(
+            u64_to_h160(1029),
+            Curve25519ScalarMul::execute as PrecompileFn,
+        );
+        set.insert(u64_to_h160(1030), Ed25519Verify::execute as PrecompileFn);
 
         Precompiles { set }
     }
@@ -45,4 +65,3 @@ impl PrecompileSet for Precompiles {
 fn u64_to_h160(index: u64) -> H160 {
     H160::from_low_u64_be(index)
 }
-
